8d511af7e2bc20b09b8785bdd3c3f5a401c497de
danil275 <antisofronov@gmail.com>
2024-03-29 14:10:42 +0500

    Update assets

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <script type="text/javascript" src="{{ asset('js/bootstrap/bootstrap.min.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap/5.3.3/bootstrap.min.css') }}">
+    <script type="text/javascript" src="{{ asset('assets/bootstrap/5.3.3/js/bootstrap.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/5.3.3/css/bootstrap.min.css') }}">
 </head>
 
 <body>
